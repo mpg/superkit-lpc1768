69a182b065c1f97f26046cb002d5ea26696ad229
Manuel Pégourié-Gonnard <mpg2@elzevir.fr>
2015-08-08 14:15:26 +0200

    Make the generated code more compact

--- baremetal/01b-blink-asm/blinky.s
+++ baremetal/01b-blink-asm/blinky.s
@@ -1,17 +1,19 @@
 @ Very minimalistic blinky program for the LPC1768
+.syntax unified
 
-@ 2.1 plus data sheet
-.set ram_bottom, 0x10000000
-.set ram_size,   0x00008000
-.set ram_top,    ram_bottom + ram_size
-
-@ interrupt vector table (34.3.3.4)
 @ For some reason, defining only the first two entries fails,
 @ so reserve space for all 16 "base" handlers
+.set nb_vtors,  16
+
+@ We don't want to use a linker (script), so compute address of main ourselves
+@ Last bit must be set to one as this is a thumb function
+.set main_addr, 4 * nb_vtors + 1
+
+@ interrupt vector table (34.3.3.4)
 vtor:
-    .word   ram_top
-    .word   0x41
-.rept 14
+    .word   0           @ we won't use the stack
+    .word   main_addr
+.rept nb_vtors - 2
     .word   0
 .endr
 
@@ -20,9 +22,10 @@ vtor:
 .set led1,  1 << 18
 
 @ 9.5 GPIO registers
-.set fio1dir,   0x2009C020
-.set fio1mask,  0x2009C030
-.set fio1pin,   0x2009C034
+.set fio1base,  0x2009C020
+.set dir_offt,  0x00
+.set mask_offt, 0x10
+.set pin_offt,  0x14
 
 @ see Cortex-M3 technical reference manual 3.3.1 and wait: below
 @ assume the unconditional branch is easy to speculate so that the pipeline
@@ -30,29 +33,25 @@ vtor:
 .set loop_cycles, 4
 
 @ initial clock source is the internal RC at about 4MHz (4.3.1)
-.set cycles_per_sec, 4000000
+@ round up to nearest power of two
+.set cycles_per_sec, 4 * 1024 * 1024
 
 @ we want a half-period of about a half second
 .set wait_reps, cycles_per_sec / loop_cycles / 2
 
 main:
-    ldr r0, =led1
-    ldr r1, =fio1dir
-    str r0, [r1]        @ led1 is output
-
-    mvn r0, r0
-    ldr r1, =fio1mask
-    str r0, [r1]        @ operations only affect led1
+    mov  r0, #led1
+    ldr  r1, =fio1base
+    str  r0, [r1, #dir_offt]    @ led1 is output (all other are input)
 
 blink:
-    mvn r0, r0          @ switch between on and off
-    ldr r1, =fio1pin
-    str r0, [r1]        @ set led1 on or off
+    str  r0, [r1, #pin_offt]    @ set led1 on or off
+    mvns r0, r0                 @ switch between on and off
 
-    ldr r1, =wait_reps
+    mov  r2, #wait_reps
 wait:
-    sub r1, #1          @ 1 cycle
-    beq blink           @ 1 cycle if not taken
-    b wait              @ 1 cycle + pipeline refill (assume 2 total)
+    subs r2, #1                 @ 1 cycle
+    beq  blink                  @ 1 cycle if not taken
+    b    wait                   @ 1 cycle + pipeline refill (assume 2 total)
 
 @ vim: ft=armasm
