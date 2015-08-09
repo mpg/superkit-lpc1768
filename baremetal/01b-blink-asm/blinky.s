53a98b3f4722e6a33819d070e658467bfb319a23
Manuel Pégourié-Gonnard <mpg2@elzevir.fr>
2015-08-09 12:00:22 +0200

    blinky.bin down to 36 bytes, reuse most of vtor

--- baremetal/01b-blink-asm/blinky.s
+++ baremetal/01b-blink-asm/blinky.s
@@ -1,14 +1,6 @@
 @ Very minimalistic blinky program for the LPC1768
 .syntax unified
 
-@ For some reason, defining only the first two entries fails,
-@ so reserve space for all 16 "base" handlers
-.set nb_vtors,  16
-
-@ We don't want to use a linker (script), so compute address of main ourselves
-@ Last bit must be set to one as this is a thumb function
-.set main_addr, 4 * nb_vtors + 1
-
 @ Schematics, sheet 3/5: LED1 is P1.18
 @ 8.3 says GPIO is the default function
 .set led1,  1 << 18
@@ -18,14 +10,6 @@
 .set dir_offt,  0x00
 .set mask_offt, 0x10
 .set pin_offt,  0x14
-
-@ interrupt vector table (34.3.3.4)
-vtor:
-    .word   fio1base    @ we won't use the stack, store something useful here
-    .word   main_addr
-.rept nb_vtors - 2
-    .word   0
-.endr
 
 @ see Cortex-M3 technical reference manual 3.3.1 and wait: below
 @ assume the unconditional branch is easy to speculate so that the pipeline
@@ -46,11 +30,16 @@ vtor:
 @ also use the fact the lr has well-defined reset value (all bits 1)
 @ to get free init for the register used to set led state
 
+@ interrupt vector table (34.3.3.4)
+@ let's play dirty and reuse most of that for our purposes,
+@ just keep some reserved registers (7-10 and 12-13) to a 0 value
+vtor:
+    .word   0                   @ not usin the stack
+    .word   0x09                @ address of main
 main:
     mov  r0, #led1
-    ldr  r1, vtor               @ value of fio1base
+    ldr  r1, iobase_addr
     str  r0, [r1, #dir_offt]    @ led1 is output (all other are input)
-
 blink:
     str  r2, [r1, #pin_offt]    @ set led1 on or off
     mvns r2, r2                 @ switch between on and off
@@ -60,5 +49,12 @@ wait:
     subs r3, #1                 @ 1 cycle
     beq  blink                  @ 1 cycle if not taken
     b    wait                   @ 1 cycle + pipeline refill (assume 2 total)
+@ this was 20 bytes, ie vtor[2] to vtor[6] included, next is vtor[7]
+@ apparently the core cares about vtor[7] being 0, so make it happy
+    .word   0
+@ apparently the core doesn't care about the rest of the reserved vtors,
+@ so just put the last address we need here and be done
+iobase_addr:
+    .word   fio1base
 
 @ vim: ft=armasm
