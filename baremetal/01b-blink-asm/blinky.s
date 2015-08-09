131a367f24745a678c9f742c0d6b9ca07573d615
Manuel Pégourié-Gonnard <mpg2@elzevir.fr>
2015-08-09 11:07:15 +0200

    Save 4 bytes on blinky.s

--- baremetal/01b-blink-asm/blinky.s
+++ baremetal/01b-blink-asm/blinky.s
@@ -30,14 +30,21 @@ vtor:
 @ see Cortex-M3 technical reference manual 3.3.1 and wait: below
 @ assume the unconditional branch is easy to speculate so that the pipeline
 @ refill is only 1 cycle most of the time
-.set loop_cycles, 4
-
+@.set loop_cycles, 4
+@
 @ initial clock source is the internal RC at about 4MHz (4.3.1)
 @ round up to nearest power of two
-.set cycles_per_sec, 4 * 1024 * 1024
-
+@.set cycles_per_sec, 4 * 1024 * 1024
+@
 @ we want a half-period of about a half second
-.set wait_reps, cycles_per_sec / loop_cycles / 2
+@.set wait_reps, cycles_per_sec / loop_cycles / 2
+@
+@ notice that the result is exactly 2 * led1
+@ we use that to save two bytes (narrow instr. vs wide isntr.)
+@ by loading the value from a register rather than immediate
+
+@ also use the fact the lr has well-defined reset value (all bits 1)
+@ to get free init for the register used to set led state
 
 main:
     mov  r0, #led1
@@ -45,12 +52,12 @@ main:
     str  r0, [r1, #dir_offt]    @ led1 is output (all other are input)
 
 blink:
-    str  r0, [r1, #pin_offt]    @ set led1 on or off
-    mvns r0, r0                 @ switch between on and off
+    str  r2, [r1, #pin_offt]    @ set led1 on or off
+    mvns r2, r2                 @ switch between on and off
 
-    mov  r2, #wait_reps
+    lsls r3, r0, #1             @ that's wait_reps :)
 wait:
-    subs r2, #1                 @ 1 cycle
+    subs r3, #1                 @ 1 cycle
     beq  blink                  @ 1 cycle if not taken
     b    wait                   @ 1 cycle + pipeline refill (assume 2 total)
 
